7613110106bf041cb6c3a62d1c8ae22567ea1ec3
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-09-01 07:17:18 +0200

    Initial Commit

---
+++ BrixieDAL/BrixieDAL.swift
@@ -0,0 +1,9 @@
+//
+//  BrixieDAL.swift
+//  BrixieDAL
+//
+//  Created by Matthias Wallner-Géhri on 01.09.25.
+//
+
+import Foundation
+
---
+++ BrixieDALTests/BrixieDALTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrixieDALTests.swift
+//  BrixieDALTests
+//
+//  Created by Matthias Wallner-Géhri on 01.09.25.
+//
+
+import Testing
+@testable import BrixieDAL
+
+struct BrixieDALTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
